fdbe13eb2e46e87ccebe6fe924f13be045459475
RYuriy <radov.yuriy@ukr.net>
2023-03-05 14:10:49 +0200

    + web-driver

--- src/test/java/tests/MyFirstTest.java
+++ src/test/java/tests/MyFirstTest.java
@@ -1,11 +1,18 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
     @Test
     public void firstTest() {
-        Assert.assertEquals(1, 1);
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        String url = "https://www.google.com.ua/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
     }
 }
