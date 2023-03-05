fe94c21a63d1e65d23c7fe71c339a241ea5d1bb1
RYuriy <radov.yuriy@ukr.net>
2023-03-05 14:28:34 +0200

    + headless mode

--- src/test/java/tests/MyFirstTest.java
+++ src/test/java/tests/MyFirstTest.java
@@ -2,17 +2,32 @@ package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
+    ChromeDriver driver;
+    FirefoxDriver firefoxDriver;
+
+    String url = "https://www.google.com.ua/";
+
+
     @Test
     public void firstTest() {
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-        String url = "https://www.google.com.ua/";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // включение headless-режима
+        driver = new ChromeDriver(options);
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+
+//        WebDriverManager.firefoxdriver().setup();
+//        firefoxDriver = new FirefoxDriver();
+//        firefoxDriver.get(url);
+//        Assert.assertEquals(firefoxDriver.getCurrentUrl(), url);
+//        firefoxDriver.quit();
     }
 }
