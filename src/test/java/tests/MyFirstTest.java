75a06c68612f18ab9f785b5a5b3c18128cf97bec
RYuriy <radov.yuriy@ukr.net>
2023-03-12 01:15:53 +0200

    + run tests in github

--- src/test/java/tests/MyFirstTest.java
+++ src/test/java/tests/MyFirstTest.java
@@ -18,11 +18,11 @@ public class MyFirstTest {
     final String TEST_PAGE_URL = "https://www.google.com.ua/";
 
 
-//    @Test
-//    public void firstTest() {
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
+    @Test
+    public void firstTest() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 //        options.setExperimentalOption("prefs", chromePrefs);
 //        options.addArguments("--disable-extensions"); // Отключает расширения Chrome
 //        options.setBinary("/usr/bin/google-chrome-stable");
@@ -32,27 +32,27 @@ public class MyFirstTest {
 //         options.addArguments("--window-size=1920x1080"); // should be enabled for Jenkins
 //         options.setCapability("seleniumAddress", "http://localhost:4444");
 //         options.setCapability("seleniumAddress", "http://localhost:4444/wd/hub");
-//        driver = new ChromeDriver(options);
-//
-//
-//         String pageUrl = System.getenv("TEST_URL");
-//         driver.get(pageUrl);
-//         Assert.assertEquals(driver.getCurrentUrl(), TEST_PAGE_URL);
-//
-//         String password = System.getenv("TEST_PASSWORD");
-//         Assert.assertEquals(password, "11111111");
-//
-//        driver.quit();
-//    }
+        driver = new ChromeDriver(options);
 
-    @Test
-    public void remoteSeleniumTest() throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
-//        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), options);
-        /** for Docker only */
-        driver = new RemoteWebDriver(new URL("http://selenium:4444"), options);
-        driver.get(TEST_PAGE_URL);
-        Assert.assertEquals(driver.getCurrentUrl(), TEST_PAGE_URL);
+
+         String pageUrl = System.getenv("TEST_URL");
+         driver.get(pageUrl);
+         Assert.assertEquals(driver.getCurrentUrl(), TEST_PAGE_URL);
+
+         String password = System.getenv("TEST_PASSWORD");
+         Assert.assertEquals(password, "11111111");
+
         driver.quit();
     }
+
+//    @Test
+//    public void remoteSeleniumTest() throws MalformedURLException {
+//        ChromeOptions options = new ChromeOptions();
+////        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), options);
+//        /** for Docker only */
+//        driver = new RemoteWebDriver(new URL("http://selenium:4444"), options);
+//        driver.get(TEST_PAGE_URL);
+//        Assert.assertEquals(driver.getCurrentUrl(), TEST_PAGE_URL);
+//        driver.quit();
+//    }
 }
